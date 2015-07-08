0bafffae6772fceb99a27f4fa7dbad391811613b
ajlopez <webmaster@ajlopez.com>
2015-07-08 18:06:47 -0300

    Refactor templateSimple test to simpleunit

--- test/templateSimple.js
+++ test/templateSimple.js
@@ -1,19 +1,29 @@
 
-var $ = require('../')
-  , assert = require('assert');
-  
-assert.equal(doTemplate("Hello, World"), "Hello, World");
+var $ = require('../');
 
-assert.equal(doTemplate("1 + 2 = ${1+2}"), "1 + 2 = 3");
-assert.equal(doTemplate("<#var x = 'My World'; #>Hello, ${x}"), "Hello, My World");
-assert.equal(doTemplate("<#for (var k=1; k<=6; k++) writer.write(k);#>"), "123456");
-assert.equal(doTemplate("<#for (var k=1; k<=6; k++) { #>${k}<# } #>"), "123456");
+exports['simple template'] = function (test) {
+	test.equal(doTemplate("Hello, World"), "Hello, World");
+} 
 
-var result = doTemplate("<#for (var k=1; k<=6; k++) { #>${k}<# } #> Current time ${new Date()}");
-assert.ok(result.indexOf("123456") >= 0);
-assert.ok(result.indexOf("Current time") >= 0);
-assert.ok(result.indexOf("#>") < 0);
+exports['simple template with expression'] = function (test) {
+	test.equal(doTemplate("1 + 2 = ${1+2}"), "1 + 2 = 3");
+}
 
+exports['simple template with variable'] = function (test) {
+	test.equal(doTemplate("<# var x = 'My World'; #>Hello, ${x}"), "Hello, My World");
+}
+
+exports['simple template with for command'] = function (test) {
+	test.equal(doTemplate("<#for (var k=1; k<=6; k++) writer.write(k);#>"), "123456");
+}
+
+exports['simple template with for command and expression'] = function (test) {
+	var result = doTemplate("<#for (var k=1; k<=6; k++) { #>${k}<# } #> Current time ${new Date()}");
+	test.ok(result.indexOf("123456") >= 0);
+	test.ok(result.indexOf("Current time") >= 0);
+	test.ok(result.indexOf("#>") < 0);
+}
+	
 function doTemplate(text)
 {
     var template = $.compileTemplate(text);

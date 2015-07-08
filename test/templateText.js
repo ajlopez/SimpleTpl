6bed8392427af65b8b6a829bfb5c0d7d4f20c4a1
ajlopez <webmaster@ajlopez.com>
2015-07-08 18:36:24 -0300

    Refactor template text test to simpleunit

--- test/templateText.js
+++ test/templateText.js
@@ -1,13 +1,13 @@
 
-var $ = require('../')
-  , assert = require('assert');
-  
-assert.ok(!$.isTemplateText());
-assert.ok(!$.isTemplateText(null));
-assert.ok(!$.isTemplateText("Hello, world"));
-assert.ok(!$.isTemplateText("<h1>Hello, world</h1>"));
+var $ = require('../');
 
-assert.ok($.isTemplateText("1 + 2 = ${1+2}"));
-assert.ok($.isTemplateText("<#var x = 'My World'; #>Hello, ${x}"));
-assert.ok($.isTemplateText("<#for (var k=1; k<=6; k++) writer.write(k);#>"));
+exports['is template text'] = function (test) {
+	test.ok(!$.isTemplateText());
+	test.ok(!$.isTemplateText(null));
+	test.ok(!$.isTemplateText("Hello, world"));
+	test.ok(!$.isTemplateText("<h1>Hello, world</h1>"));
 
+	test.ok($.isTemplateText("1 + 2 = ${1+2}"));
+	test.ok($.isTemplateText("<#var x = 'My World'; #>Hello, ${x}"));
+	test.ok($.isTemplateText("<#for (var k=1; k<=6; k++) writer.write(k);#>"));
+};  
